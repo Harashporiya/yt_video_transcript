66a22b4e1a9ef2d2958371aa697846aa9f947059
Harash Poriya <harash.poriya@gmail.com>
2026-05-20 14:04:20 +0530

    user routes

---
+++ routes/user.routes.js
@@ -0,0 +1,9 @@
+import { signUp,login } from "../controllers/user.controllers.js";
+import { Router } from "express";
+
+const router=Router();
+
+router.post("/signup",signUp);
+router.post("/login",login);
+
+export default router;
