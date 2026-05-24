925bf8ca10688a29a1494a84eb682d75ea24f6c8
Harash Poriya <harash.poriya@gmail.com>
2026-05-24 09:47:37 +0530

    Explicitly handle CORS preflight

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
+const corsOptions = {
   origin: ["http://localhost:3001", "http://localhost:3000"],
-  methods: ["GET","POST","DELETE","PUT","PATCH"],
+  methods: ["GET","POST","DELETE","PUT","PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
-}));
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
