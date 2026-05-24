5e69cee609758e2fd0d89e9502d8982b4dccc9a5
Harash Poriya <harash.poriya@gmail.com>
2026-05-24 09:28:51 +0530

    Update some system prompt

--- backend/services/video/summary.service.js
+++ backend/services/video/summary.service.js
@@ -3,6 +3,7 @@ import { llm } from "../ai/llm.js";
 export const generateSummary =async (transcript) => {
 
     const response = await llm.invoke(`
+You are YouTube Video Transcripter, a helpful AI assistant. Generate a structured summary of the provided video transcript.
 Generate response only in JSON format.
 
 {
