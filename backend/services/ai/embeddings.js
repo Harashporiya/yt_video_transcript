a4168df443083edc879dbed4c5f14af62f936bd1
Harash Poriya <harash.poriya@gmail.com>
2026-05-26 23:26:00 +0530

    Remove the llm google gemini add groq and huggingface model for embedding

--- backend/services/ai/embeddings.js
+++ backend/services/ai/embeddings.js
@@ -1,6 +1,12 @@
-import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+// import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
 
-export const embeddings = new GoogleGenerativeAIEmbeddings({
-    apiKey:process.env.GOOGLE_GEMINI_API_KEY,
-    model: "gemini-embedding-001",
+// export const embeddings = new GoogleGenerativeAIEmbeddings({
+//     apiKey:process.env.GOOGLE_GEMINI_API_KEY,
+//     model: "gemini-embedding-001",
+// });
+
+export const embeddings = new HuggingFaceInferenceEmbeddings({
+    apiKey: process.env.HUGGINGFACE_API_KEY,
+    model: "sentence-transformers/all-MiniLM-L6-v2",
 });
--- backend/services/ai/llm.js
+++ backend/services/ai/llm.js
@@ -1,8 +1,7 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatGroq } from "@langchain/groq";
 
-export const llm = new ChatGoogleGenerativeAI({
-    apiKey: process.env.GOOGLE_GEMINI_API_KEY,
-    model: "gemini-2.5-flash-lite",
-    // model:"gemini-3.1-flash-lite-preview",
+export const llm = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY,
+    model: "llama-3.3-70b-versatile",
     temperature: 0.1,
 });
