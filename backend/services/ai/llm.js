e4cf928c8043499fbd4c3a39b3c947ae4dcb1dd3
Harash Poriya <harash.poriya@gmail.com>
2026-05-30 15:07:25 +0530

    Change llm and embedding

--- backend/services/ai/llm.js
+++ backend/services/ai/llm.js
@@ -1,8 +1,17 @@
 import "dotenv/config";
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatGroq } from "@langchain/groq";
 
-export const llm = new ChatGoogleGenerativeAI({
-    apiKey: process.env.GOOGLE_GEMINI_API_KEY,
-    model: "gemini-3.1-flash-lite",
+export const llm = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY,
+    model: "llama-3.3-70b-versatile",
     temperature: 0.1,
 });
+
+
+// import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+// export const llm = new ChatGoogleGenerativeAI({
+//     apiKey: process.env.GOOGLE_GEMINI_API_KEY,
+//     model: "gemini-2.5-flash-lite",
+//     temperature: 0.1,
+// });
