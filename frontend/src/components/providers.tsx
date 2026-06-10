a3e17a77ee8fc8377c50570805c522c31d721d49
Harash Poriya <harash.poriya@gmail.com>
2026-06-10 17:31:58 +0530

    Add redux store

--- frontend/src/components/providers.tsx
+++ frontend/src/components/providers.tsx
@@ -1,10 +1,14 @@
 "use client"
 import { SessionProvider } from "next-auth/react"
+import { Provider as ReduxProvider } from "react-redux"
+import { store } from "@/store/store"
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <SessionProvider>
-            {children}
-        </SessionProvider>
+        <ReduxProvider store={store}>
+            <SessionProvider>
+                {children}
+            </SessionProvider>
+        </ReduxProvider>
     )
 }
